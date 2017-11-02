7818a382deae38070d9af3eac1d85e27615b9bee
Aayush <aayushjaiswal0114@gmail.com>
2017-11-02 22:27:09 +0530

    Student models created

--- models/student.js
+++ models/student.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const connect = mongoose.connect('mongodb://localhost/student_details');
-const db = mongoose.connection();
+const db = mongoose.connection;
 var StudentSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true},
