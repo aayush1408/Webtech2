4f50c2b81e411d518fc024e612d479f8dc0d9c09
Aayush <aayushjaiswal0114@gmail.com>
2017-11-04 10:41:28 +0530

    Teacher model created

---
+++ models/teacher.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const connect = mongoose.connect('mongodb://localhost/student_details');
+const db = mongoose.connection;
+var TeacherSchema = new mongoose.Schema({
+    username:{type:String},
+    password:{type:String}
+});
+
+ module.exports = mongoose.model('Teacher',TeacherSchema); 
